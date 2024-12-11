ff2f5b9b9b94224cbe3f8bb514f6217d3e91a148
ВарКомНадзор <artemdubrovin2artem@yandex.ru>
2024-12-12 00:02:54 +0300

    Update DTU.py

--- DTU.py
+++ DTU.py
@@ -1,4 +1,4 @@
-print("When running this program, it combines all the separated DDS images in the directory into whole images.\nDeveloper: ВарКомНадзор (wfom)\nLink to the group in VKontakte: https://vk.com/wf_module \nLink to the group in Telegram: https://t.me/+ASA1xbLPnIQ0ZDUy \nLink to GitHub: https://github.com/wfom \nProgram version: v0.1")
+print("When running this program, it combines all the separated DDS images in the directory into whole images.\nDeveloper: ВарКомНадзор (wfom)\nLink to the group in VKontakte: https://vk.com/wf_module \nLink to the group in Telegram: https://t.me/+ASA1xbLPnIQ0ZDUy \nLink to GitHub: https://github.com/wfom \nProgram version: v0.2")
 
 import os
 
@@ -6,23 +6,9 @@ def combine_dds_files(input_dds):
     dds_null = input_dds
     name_dds = input_dds[:-2].split("\\")[-1]
     filename = input_dds[:-2]
+    number = [16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
     files = []
-    files += [f for f in all if f.endswith(f'{name_dds}.16')]
-    files += [f for f in all if f.endswith(f'{name_dds}.15')]
-    files += [f for f in all if f.endswith(f'{name_dds}.14')]
-    files += [f for f in all if f.endswith(f'{name_dds}.13')]
-    files += [f for f in all if f.endswith(f'{name_dds}.12')]
-    files += [f for f in all if f.endswith(f'{name_dds}.11')]
-    files += [f for f in all if f.endswith(f'{name_dds}.10')]
-    files += [f for f in all if f.endswith(f'{name_dds}.9')]
-    files += [f for f in all if f.endswith(f'{name_dds}.8')]
-    files += [f for f in all if f.endswith(f'{name_dds}.7')]
-    files += [f for f in all if f.endswith(f'{name_dds}.6')]
-    files += [f for f in all if f.endswith(f'{name_dds}.5')]
-    files += [f for f in all if f.endswith(f'{name_dds}.4')]
-    files += [f for f in all if f.endswith(f'{name_dds}.3')]
-    files += [f for f in all if f.endswith(f'{name_dds}.2')]
-    files += [f for f in all if f.endswith(f'{name_dds}.1')]
+    files += [f for f in all if f.endswith(f'{name_dds}.{[n for n in number]}')]
     with open(dds_null, 'rb') as first_file:
         start = first_file.read(128)
         end = first_file.read()
@@ -35,9 +21,19 @@ def combine_dds_files(input_dds):
         outfile.write(data)
         print(f'Merging of the [{filename}] file has been completed successfully')
 
+def all_files():
+    all = []
+    current_dir = os.getcwd()
+    print(f'Working directory: {current_dir}')
+    for root, dirs, files in os.walk(current_dir):
+        for file in files:
+            full_path = os.path.join(root, file)
+            all.append(full_path)
+    return all
+
 def find_files_with_extension():
     all_dds0 = []
-    current_dir = os.path.dirname(os.path.abspath(__file__))
+    current_dir = os.getcwd()
     for root, dirs, files in os.walk(current_dir):
         for file in files:
             if file.endswith('.dds.0'):
@@ -45,15 +41,6 @@ def find_files_with_extension():
                 all_dds0.append(full_path)
                 
     return all_dds0
-
-def all_files():
-    all = []
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    for root, dirs, files in os.walk(current_dir):
-        for file in files:
-            full_path = os.path.join(root, file)
-            all.append(full_path)
-    return all
 
 all = all_files()
 all_dds0 = find_files_with_extension()
